5c573044180683b788318bf3e9f5e85583cf2822
ignacioaurre <ignacio@aurrecoechea.com.ar>
2020-06-27 00:24:38 -0300

    Actualizado hasta middlewares y session

---
+++ src/middlewares/auth.js
@@ -0,0 +1,9 @@
+
+function authMiddleware(req, res, next) {
+    if (req.session.userId == undefined) {
+        return res.redirect('/users/login')
+    }
+    next()
+}
+
+module.exports = authMiddleware
---
+++ src/routes/main.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+var express = require('express');
+var router = express.Router();
+const authMiddleware = require('../middlewares/auth')
+let { check, validationResult, body} = require('express-validator');
+let adminMiddleware = require('../middlewares/adminMiddleware')
+
+const mainController = require('../controllers/mainController')
+
+/* GET home page. */
+router.get('/', mainController.home)
+router.get('/ofertas', mainController.ofertas)
+router.get('/admin', adminMiddleware, mainController.admin)
+
+module.exports = router;
